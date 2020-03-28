6d7859299ac1e4e63cade703ede91bdb1ba075fc
taha ansari <taha.m.ansari@gmail.com>
2020-03-28 14:08:57 +0530

    Updates

---
+++ precache-manifest.31c1c1594b13f8cf2a72d728ee602305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d942c54640290e43c81499deb47469",
+    "url": "/myfirstreact/index.html"
+  },
+  {
+    "revision": "f5ad772e7fdca28876bd",
+    "url": "/myfirstreact/static/js/2.32c12192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myfirstreact/static/js/2.32c12192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad95eb971057c848a2b9",
+    "url": "/myfirstreact/static/js/main.e1142452.chunk.js"
+  },
+  {
+    "revision": "da1ea6875331a2748f30",
+    "url": "/myfirstreact/static/js/runtime-main.758bda30.js"
+  }
+]);
