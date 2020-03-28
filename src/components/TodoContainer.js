1c05d14424a45b5f02fc503e9d51d3a2a7c73987
taha ansari <taha.m.ansari@gmail.com>
2020-03-28 13:58:35 +0530

    initial commit

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class TodoContainer extends React.Component{
+    render(){
+        return (
+            <>
+                <h1>Helllo, I am the parent component</h1>
+            </>
+        )
+    }
+}
+export default TodoContainer;
